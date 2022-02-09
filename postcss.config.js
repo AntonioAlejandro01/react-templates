022521d1f6bb84fbbfd1cfd65e9da30f470cee83
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2022-02-09 13:37:07 +0100

    :bug: [BUG postcss]

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
 
 module.exports = {
-	plugins: [
-		require('tailwindcss')('./tailwind.config.js'),
-		require('autoprefixer'),
-		purgecss({
-			content: ['./**/*.html'],
-		}),
-	],
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    purgecss({
+      content: ['./**/*.html', './**/*.jsx'],
+    }),
+  ],
 };
